402f0faad142a28c2325f733f4173175d8a1c4df
pamsi2014ksokolowski <200510@student.pwr.edu.pl>
2014-05-06 12:26:09 +0200

    Program 5 - Grafy. Koncowa wersja
    Koncowa wersja

--- pamsi2014ksokolowski/Program 5 - Grafy/funkcje.cpp
+++ pamsi2014ksokolowski/Program 5 - Grafy/funkcje.cpp
@@ -33,3 +33,64 @@ double wylosuj_prawdop_min3(int n)
 void funkcja(Drzewo<int> moje){
 	cout << endl;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void menu()
+{
+	char wybor;
+	Drzewo<int> minST;
+	while (true){
+		cout << "\n\n******Wybierz opcje" << endl
+			<< "1. Wczytaj graf z pliku." << endl
+			<< "2. Zapisz graf do pliku. " << endl
+			<< "3. Stworz MST algorytmem Kruskala." << endl
+			<< "4. Stworz MST algorytmem Prima." << endl
+			<< "5. Stworz MST algorytmem Boruvki." << endl
+			<< "6. Wyswietl graf jako liste." << endl
+			<< "7. Wyswietl graf jako tablice." << endl
+			<< "0. Wyjscie z programu." << endl;
+
+		do{
+			cout << "\rTwoj wybor: ";
+			wybor = _getch();
+		} while (wybor < '0' || wybor > '7');
+
+		cout << endl;
+		switch (wybor){
+
+		case '1': {		cout << "\nPodaj sciezke do pliku:\t";
+						string sciezka;
+						getline(cin, sciezka);
+						minST.wczytaj_graf_z_pliku(sciezka);
+						break; }
+		case '2': {		cout << "\nPodaj sciezke do pliku:\t";
+						string sciezka;
+						getline(cin, sciezka);
+						minST.zapisz_graf_do_pliku(sciezka);
+						break; }
+		case '3': {		if (minST.pusty()){
+						  cout << "\nGraf jest pusty!" << endl;
+						  break;
+						}
+						  minST.alg_Kruskala();
+						  break; }
+		case '4': {		if (minST.pusty()){
+						  cout << "\nGraf jest pusty!" << endl;
+						  break;
+						}
+						  minST.alg_Prima();
+						  break; }
+		case '5': {		if (minST.pusty()){
+						  cout << "\nGraf jest pusty!" << endl;
+						  break;
+						}
+						  minST.alg_Boruvki();
+						  break; }
+		case '6': {		minST.wyswietl_graf_lista();
+						break; }
+		case '7': {		minST.wyswietl_graf_tablica();
+						break; }
+		case '0': {		return; }
+		}
+	}
+}
--- pamsi2014ksokolowski/Program 5 - Grafy/main.cpp
+++ pamsi2014ksokolowski/Program 5 - Grafy/main.cpp
@@ -7,7 +7,13 @@
 
 using namespace std;
 
-
+//////////////////////////////////////////////////////////////////////////////////////////
+//Wykonywanie algorytmu Kruskala, Prima, Boruvki 'l_elementow' razy pobranie czasu
+//kazdorazowego wykonania algorytmu.
+//Argumenty: tab[] - tablica na drzewa na ktorych ma byc wykonany algorytm
+//			 funkcja - znak odpowiadajacy interesujacego nas algorytmu: K, P, B
+//			 l_elementow - rozmiar tablicy tab
+//Zwraca: sredni czas wykonania algorytmu na 'l_elementow' elementach
 template <class TYP>
 double pobierz_sredni_czas(Drzewo<TYP> tab[], const char funkcja, const int l_elementow = 100)
 {
@@ -15,47 +21,39 @@ double pobierz_sredni_czas(Drzewo<TYP> tab[], const char funkcja, const int l_el
 	int ile = l_elementow;
 
 	while (ile--){
-		std::cout << ile << " ";
+		std::cout << ile << funkcja <<" ";
 		switch (funkcja){
-		case 'K' : { tab[ile].alg_Kruskala();
-				break; }
-		case 'P' : { tab[ile].alg_Prima();
-				break; }
-		case 'B' : { tab[ile].alg_Boruvki();
-				break; }
+			case 'K': {  tab[ile].alg_Kruskala();
+					break;	 }
+			case 'P' : { tab[ile].alg_Prima();
+					break; }
+			case 'B' : { tab[ile].alg_Boruvki();
+					break; }
 		}
-		suma_czasow += tab[ile].czas_sort();
+		suma_czasow += tab[ile]._czas();
 	}
-
 	return (suma_czasow / l_elementow);
 }
-
+//////////////////////////////////////////////////////////////////////////////////////////
+//Eksperyment majacy na celu zebraniu srednich czasow wykonan dla algorytmow tworzacych MST
+//z grafow o roznej gestosci. Eksperyment wykonywany jest dla roznej liczby wierzcholkow, a
+//jego wyniki sa zapisywane do pliku
 void eksperyment()
 {
 	ofstream plik("eksperyment.txt");
 
-	const int l_elementow = 20;
-	int liczba_wierz[] = { 500, 1000, 5000, 10000 };
+	const int l_elementow = 100;
+	int liczba_wierz[] = { 100, 250, 500, 1000 };
 	Drzewo<int> tablica[l_elementow];
 
 	plik << "Typ wygen Grafu\t Liczba ele\t Kruskala\t Prima\t Boruvki\n";
 
 	int i, j;
 	
-	for (i = 0; i < 1; ++i){
+	//eksperyment dla grafu ER z prawdopodobienstwem z log...
+	for (i = 0; i < 4; ++i){
 		for (j = 0; j < l_elementow; ++j){
-			tablica[j].usun_wszystko();
-			tablica[j].graf_pelny(liczba_wierz[i]);
-			std::cout << " G"<< j;
-		}
-		plik << "Graf Pelny\t";
-		plik << liczba_wierz[i] << "\t" << pobierz_sredni_czas(tablica, 'K', l_elementow)
-								<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
-								<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
-	}
-	for (i = 0; i < 1; ++i){
-		for (j = 0; j < l_elementow; ++j){
-			std::cout << " G" << j;
+			std::cout << " Glog" << j;
 			tablica[j].usun_wszystko();
 			tablica[j].graf_ER(liczba_wierz[i], wylosuj_prawdop_log2(liczba_wierz[i]));
 			if (!tablica[j].czy_spojny()){
@@ -65,12 +63,13 @@ void eksperyment()
 		}
 		plik << "Graf ER log2\t";
 		plik << liczba_wierz[i] << "\t" << pobierz_sredni_czas(tablica, 'K', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
+								<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
+								<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
 	}
-	for (i = 0; i < 1; ++i){
+	//eksperyment dla grafu ER z prawdopodobienstwem z n^(-1/2)
+	for (i = 0; i < 4; ++i){
 		for (j = 0; j < l_elementow; ++j){
-			std::cout << " G" << j;
+			std::cout << " G2m" << j;
 			tablica[j].usun_wszystko();
 			tablica[j].graf_ER(liczba_wierz[i], wylosuj_prawdop_min2(liczba_wierz[i]));
 			if (!tablica[j].czy_spojny()){
@@ -80,12 +79,13 @@ void eksperyment()
 		}
 		plik << "Graf ER min2\t";
 		plik << liczba_wierz[i] << "\t" << pobierz_sredni_czas(tablica, 'K', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
+								<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
+								<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
 	}
-	for (i = 0; i < 1; ++i){
+	//eksperyment dla grafu ER z prawdopodobienstwem z n^(-1/3)
+	for (i = 0; i < 4; ++i){
 		for (j = 0; j < l_elementow; ++j){
-			std::cout << " G" << j;
+			std::cout << " G3m" << j;
 			tablica[j].usun_wszystko();
 			tablica[j].graf_ER(liczba_wierz[i], wylosuj_prawdop_min3(liczba_wierz[i]));
 			if (!tablica[j].czy_spojny()){
@@ -95,17 +95,28 @@ void eksperyment()
 		}
 		plik << "Graf ER min3\t";
 		plik << liczba_wierz[i] << "\t" << pobierz_sredni_czas(tablica, 'K', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
-			<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
+										<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
+										<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
+	}
+	//eksperyment dla grafu pelnego
+	for (i = 0; i < 4; ++i){
+		for (j = 0; j < l_elementow; ++j){
+			tablica[j].usun_wszystko();
+			tablica[j].graf_pelny(liczba_wierz[i]);
+			std::cout << " Gp" << j;
+		}
+		plik << "Graf Pelny\t";
+		plik << liczba_wierz[i] << "\t" << pobierz_sredni_czas(tablica, 'K', l_elementow)
+								<< "\t" << pobierz_sredni_czas(tablica, 'P', l_elementow)
+								<< "\t" << pobierz_sredni_czas(tablica, 'B', l_elementow) << endl;
 	}
 	plik.close();
 }
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-	eksperyment();
+	menu();
 
-	system("PAUSE");
 	return 0;
 }
