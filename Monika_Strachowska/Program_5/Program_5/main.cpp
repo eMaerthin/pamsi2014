8cb0ba259ac4040339c60ec00a2f87802427b1f4
Monika Magdalena Strachowska <monikastrachowska@gmail.com>
2014-05-20 13:45:33 +0200

    Ostateczna wersja prima i kruskala

--- Monika_Strachowska/Program_5/Program_5/main.cpp
+++ Monika_Strachowska/Program_5/Program_5/main.cpp
@@ -1,127 +1,190 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include <iomanip>
+#include <vector>
 #include <queue>
-#include <set>
+#include <Windows.h>
+#include <cmath>
+#include <random>
+#include <string>
 
 using namespace std;
+class Krawedz;
+class KrawedzPorownanie;
+typedef priority_queue<Krawedz,vector<Krawedz>,KrawedzPorownanie> kolejkaKP;
+Krawedz **macierz = NULL;
+vector< vector< Krawedz>> lista;
+int liczbaWierzcholkow = 0;
+int liczbaKrawedzi = 0;
+bool czyWyswietlacWyniki = false;
+
+void wczytywanieZPliku(string);
+void wyswietlanieWczytanych();
+int primLista();
+int primMacierz();
+int kruskalLista();
+int kruskalMacierz();
+kolejkaKP przepisanie(kolejkaKP);
+void usuwanieZKolejki(kolejkaKP *, int);
+int znajdz(int, int *);
+void losowanieGrafow(double);
+void wyswietlaniePolaczen(vector <Krawedz>);
+void eksperyment();
+void usrednianieCzasu(vector <long double>);
+void ustawianieFlagPrim();
+void zapisanieLosowychGrafow();
 
 class Krawedz {
 public:
-	int wagaKrawedzi;
-	int wierzcholekWej;
-	int wierzcholekWyj;
+	int win, wout, waga;
 	bool odwiedzony;
-	Krawedz(int waga, int wej, int wyj): wagaKrawedzi(waga), wierzcholekWej(wej), wierzcholekWyj(wyj), odwiedzony(false) {};
+	Krawedz():waga(-1), win(-1), wout(-1), odwiedzony(true){};
+	Krawedz(int wi, int wo, int w): win(wi), wout(wo), waga(w), odwiedzony(false) {};
 };
-
 class KrawedzPorownanie {
 public:
 	bool operator() (const Krawedz &k1, const Krawedz &k2) {
-		if(k1.wagaKrawedzi > k2.wagaKrawedzi) return true;
+		if(k1.waga > k2.waga) return true;
 		return false;
 	}
 };
 
-void wczytywanieZPliku(string nazwa, vector <Krawedz> &wektor, int *liczbaWierzcholkow, int *liczbaKrawedzi) {
+
+
+int main() {
+	int wybor = 0;
+	string nazwa;
+	do {
+		cout << "Co chcesz zrobic?\n"
+			"1 - eksperyment\n"
+			"2 - odczyt z pliku\n"
+			"3 - zapis do pliku\n"
+			"0 - wyjscie\n";
+		cin >> wybor;
+		if(wybor == 1)
+			eksperyment();
+		if(wybor == 2) {
+			cout << "Z jakiego pliku czytac (*.txt)\n";
+			cin >> nazwa;
+			wczytywanieZPliku(nazwa);
+			int z = 0;
+			cout << "Czy wyswietlic polaczenia? (1)\n";
+			cin >> z;
+			if(z == 1) czyWyswietlacWyniki = true;
+			cout << "\nPRIM LISTA\n";
+			primLista();
+			cout << "\nPRIM MACIERZ\n";
+			primMacierz();
+			cout << "\nKRUSKAL LISTA\n";
+			kruskalLista();
+			cout << "\nKRUSKAL MACIERZ\n";
+			kruskalMacierz();
+			czyWyswietlacWyniki = false;
+		}
+		if(wybor == 3) 
+			zapisanieLosowychGrafow();
+	}while( wybor != 0);
+	system("PAUSE");
+	return 0;
+}
+
+void wczytywanieZPliku(string nazwa) {
 	fstream plik(nazwa, ios::in);
 	int w, win, wout;
 	if(plik.good()) {
 		do {
 			plik >> win >> wout >> w;
-			Krawedz Ktmp1(w, win, wout);
-			Krawedz Ktmp2(w, wout, win);
-			wektor.push_back(Ktmp1);
-			wektor.push_back(Ktmp2);
-			(*liczbaWierzcholkow < win) ? (*liczbaWierzcholkow = win):0;
-			(*liczbaWierzcholkow < wout) ? (*liczbaWierzcholkow = wout):0;
+			(liczbaWierzcholkow < win) ? (liczbaWierzcholkow = win):0;
+			(liczbaWierzcholkow < wout) ? (liczbaWierzcholkow = wout):0;
+			(liczbaKrawedzi)++;
 		}while(!plik.eof());
 	}
 	plik.close();
-	*liczbaWierzcholkow += 1;
-	*liczbaKrawedzi = wektor.size()/2; // bo dwukierunkowe krawedzie
+	liczbaWierzcholkow += 1;
+
+	macierz = new Krawedz*[liczbaWierzcholkow];
+	for(int i = 0; i < liczbaWierzcholkow; i++)
+		macierz[i] = new Krawedz[liczbaWierzcholkow];
+	lista = vector< vector< Krawedz>>(liczbaWierzcholkow, vector<Krawedz> (0));
+
+	plik.open(nazwa);
+	if(plik.good()) {
+		do {
+			plik >> win >> wout >> w;
+			Krawedz k(win,wout,w);
+			Krawedz k1(wout,win,w);
+			macierz[win][wout] = k;
+			macierz[wout][win] = k1;
+			lista[win].push_back(k);
+			lista[wout].push_back(k1);
+		}while(!plik.eof());
+	}
+	plik.close();
 }
 
-void wyswietlanieMacierzy( vector <Krawedz> &wektor, int liczbaWierzcholkow) {
-	int **macierz = new int*[liczbaWierzcholkow];
-	for(int i = 0; i < liczbaWierzcholkow; i++)
-		macierz[i] = new int[liczbaWierzcholkow];
-	for(int i = 0; i < liczbaWierzcholkow; i++)
-		for(int j = 0; j < liczbaWierzcholkow; j++) 
-			macierz[i][j] = -1;
-	for(int i = 0; i < (int)wektor.size(); i++)
-		macierz[wektor[i].wierzcholekWej][wektor[i].wierzcholekWyj] = wektor[i].wagaKrawedzi;
-	cout << "\n\nMACIERZ\n\n";
+void wyswietlanieWczytanych() {
+	cout << endl;
 	for(int i = 0; i < liczbaWierzcholkow; i++) {
 		for(int j = 0; j < liczbaWierzcholkow; j++)
-			cout << setw(3) << macierz[i][j] << " ";
+			cout << setw(3) << macierz[i][j].waga << " ";
 		cout << endl;
 	}
-
-}
-
-void wyswietlanieLista(vector <Krawedz> &wektor, int liczbaWierzcholkow) {
-	vector< vector< pair<int,int>>> lista(liczbaWierzcholkow, vector<pair<int,int>> (0));
-	for(int i = 0; i < (int)wektor.size(); i++) 
-		lista[wektor[i].wierzcholekWej].push_back(make_pair(wektor[i].wierzcholekWyj,wektor[i].wagaKrawedzi));
-	cout << "\n\nLISTA\n\n";
+	cout << endl;
 	for(int i = 0; i < (int) lista.size(); i++) {
 		cout << "Wierzcholek: " << i << "\t";
-		for(int j = 0; j < lista[i].size(); j++) {
-			pair<int,int> tmp = lista[i][j];
-			cout << tmp.first << " (" << tmp.second << "); ";
+		for(int j = 0; j < (int)lista[i].size(); j++) {
+			cout << lista[i][j].wout << " (" << lista[i][j].waga << "); ";
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
-void wyswietlaniePolaczenWyniku(vector <Krawedz> wektor) {
-	for(int i = 0; i < wektor.size(); i++)
-		cout << wektor[i].wierzcholekWej << " <-> " << wektor[i].wierzcholekWyj << "  (" << wektor[i].wagaKrawedzi << ")\n";
+void wyswietlaniePolaczen(vector <Krawedz> wyniki) {
+	for(int i = 0; i < (int) wyniki.size(); i++)
+		cout << wyniki[i].win << "  -  " << wyniki[i].wout << "    => " << wyniki[i].waga << endl;
 }
 
-int znajdz(int x, int *parent) {
-	if(x != parent[x])
-		parent[x] = znajdz(parent[x], parent);
-	return parent[x];
-}
+int primLista() {
+	int aktualny = 0, suma = 0;
+	vector< vector< Krawedz>> listaTMP = lista;
 
-typedef priority_queue<Krawedz,vector<Krawedz>,KrawedzPorownanie> kolejkaKP;
-vector<Krawedz> kruskal(vector <Krawedz> wektor, int liczbaWierzcholkow) {
 	kolejkaKP kolejka;
-	for(int  i = 0; i < (int) wektor.size(); i++)
-		kolejka.push(wektor[i]);
-
 	vector <Krawedz> wynik;
-	int pIn, pOut;
-	int *parent = new int[liczbaWierzcholkow];
-	for(int i = 0; i < liczbaWierzcholkow; i++) parent[i] = i;
-	while(!kolejka.empty()) {
-		Krawedz k = kolejka.top();
-		pIn = znajdz(k.wierzcholekWej, parent);
-		pOut = znajdz(k.wierzcholekWyj, parent);
-		if(pIn != pOut) {
-			wynik.push_back(k);
-			parent[pIn] = parent[pOut];
+	int liczbaPotrzebnychKrawedzi = liczbaWierzcholkow - 1;
+	do {
+		for(int i = 0; i < liczbaWierzcholkow; i++) {
+			for(int j = 0; j < (int)lista[i].size(); j++) {
+				if(listaTMP[i][j].win == aktualny && listaTMP[i][j].odwiedzony == false)
+					kolejka.push(listaTMP[i][j]);
+				if(listaTMP[i][j].wout == aktualny)
+					listaTMP[i][j].odwiedzony = true;
+			}
 		}
-		kolejka.pop();
+		usuwanieZKolejki(&kolejka,aktualny);
+		if(!kolejka.empty()) {
+			while(kolejka.top().odwiedzony == true) kolejka.pop();
+			if(kolejka.top().odwiedzony == false) {
+				aktualny = kolejka.top().wout;
+				wynik.push_back(kolejka.top());
+				suma +=kolejka.top().waga;
+				kolejka.pop();
+			}
+		}
+		liczbaPotrzebnychKrawedzi--;
+	}while(liczbaPotrzebnychKrawedzi > 0);
+
+	if(czyWyswietlacWyniki == true) {
+		wyswietlaniePolaczen(wynik);
+		cout << "SUMA: " << suma << endl;
 	}
-	int suma = 0;
-	for(int i = 0; i < wynik.size(); i++)
-		suma += wynik[i].wagaKrawedzi;
-	cout << "SUMA KRUSKAL: " << suma << endl;
-	return wynik;
+	return suma;
 }
 
-void wyswietlanieKolejki(kolejkaKP kolejka) {
-	int rozmiar = kolejka.size();
-	for(int i = 0; i < rozmiar; i++) {
-		cout << kolejka.top().wagaKrawedzi << "   " <<
-			kolejka.top().wierzcholekWej << "   " <<
-			kolejka.top().wierzcholekWyj << "   flaga " << 
-			kolejka.top().odwiedzony << endl;
-		kolejka.pop();
+void ustawianieFlagPrim() {
+	for(int i = 0; i < liczbaWierzcholkow; i++) {
+		for(int j = 0; j < liczbaWierzcholkow; j++)
+			macierz[i][j].odwiedzony = false;
 	}
 }
 
@@ -130,14 +193,14 @@ kolejkaKP przepisanie(kolejkaKP tmp) {
 		kolejkaKP kolejka;
 		int rozmiar = tmp.size();
 		for(int i = 0; i < rozmiar; i++) {
-			Krawedz k(tmp.top().wagaKrawedzi,tmp.top().wierzcholekWej,tmp.top().wierzcholekWyj);
+			Krawedz k(tmp.top().win,tmp.top().wout,tmp.top().waga);
 			kolejka.push(k);
 			tmp.pop();
 		}
 		return kolejka;
 	}
 	else {
-		cout << "\nPusta kolejka do przepisania\n";
+		cout << "\nGraf nie pelny\n";
 		system("PAUSE");
 		exit(-1);
 	}
@@ -149,7 +212,7 @@ void usuwanieZKolejki(kolejkaKP *kolejka, int wOut) {
 	int rozmiar = (*kolejka).size();
 	while(!(*kolejka).empty()) (*kolejka).pop();
 	for(int i = 0; i < rozmiar; i++) {
-		if(tmp.top().wierzcholekWyj == wOut)
+		if(tmp.top().wout == wOut)
 			tmp.pop();
 		else {
 			(*kolejka).push(tmp.top());
@@ -158,23 +221,28 @@ void usuwanieZKolejki(kolejkaKP *kolejka, int wOut) {
 	}
 }
 
-vector<Krawedz> prim(vector <Krawedz> wektor, int liczbaWierzcholkow) {
-	int aktualny = 0, suma = 0, rozmiar = wektor.size();
+int primMacierz() {
+	int aktualny = 0, suma = 0;
+	Krawedz **macierzTMP = macierz;
+
 	kolejkaKP kolejka;
 	vector <Krawedz> wynik;
 	int liczbaPotrzebnychKrawedzi = liczbaWierzcholkow - 1;
 	do {
-		for(int i = 0; i < rozmiar; i++) {
-			if(wektor[i].wierzcholekWej == aktualny && wektor[i].odwiedzony == false)
-				kolejka.push(wektor[i]);
-			if(wektor[i].wierzcholekWyj == aktualny)
-				wektor[i].odwiedzony = true;
+		for(int i = 0; i < liczbaWierzcholkow; i++) {
+			for(int j = 0; j < liczbaWierzcholkow; j++) {
+				if(macierzTMP[i][j].win == aktualny && macierzTMP[i][j].odwiedzony == false)
+					kolejka.push(macierzTMP[i][j]);
+				if(macierzTMP[i][j].wout == aktualny)
+					macierzTMP[i][j].odwiedzony = true;
+			}
 		}
 		usuwanieZKolejki(&kolejka,aktualny);
 		if(!kolejka.empty()) {
 			while(kolejka.top().odwiedzony == true) kolejka.pop();
 			if(kolejka.top().odwiedzony == false) {
-				aktualny = kolejka.top().wierzcholekWyj;
+				aktualny = kolejka.top().wout;
+				suma += kolejka.top().waga;
 				wynik.push_back(kolejka.top());
 				kolejka.pop();
 			}
@@ -182,22 +250,212 @@ vector<Krawedz> prim(vector <Krawedz> wektor, int liczbaWierzcholkow) {
 		liczbaPotrzebnychKrawedzi--;
 	}while(liczbaPotrzebnychKrawedzi > 0);
 
-	for(int i = 0; i < wynik.size(); i++ )
-		suma += wynik[i].wagaKrawedzi;
-	cout << "SUMA PRIM: " << suma << endl;
-	return wynik;
+	if(czyWyswietlacWyniki == true) {
+		wyswietlaniePolaczen(wynik);
+		cout << "SUMA: " << suma << endl;
+	}
+
+	return suma;
 }
 
-int main() {
-	int liczbaKrawedzi = 0, liczbaWierzcholkow = 0;
-	vector <Krawedz> wektor;
-	cout << liczbaKrawedzi << " " << liczbaWierzcholkow << endl;
-	wczytywanieZPliku("graf10-100.txt", wektor, &liczbaWierzcholkow, &liczbaKrawedzi);
-	cout << liczbaKrawedzi << " " << liczbaWierzcholkow << endl;
-	wyswietlanieMacierzy(wektor,liczbaWierzcholkow);
-	wyswietlanieLista(wektor,liczbaWierzcholkow);
-	wyswietlaniePolaczenWyniku(kruskal(wektor,liczbaWierzcholkow));
-	prim(wektor,liczbaWierzcholkow);
-	system("PAUSE");
-	return 0;
+int kruskalLista() {
+	vector< vector< Krawedz>> listaTMP = lista;
+	kolejkaKP kolejka;
+	for(int  i = 0; i < liczbaWierzcholkow; i++) {
+		for(int j = 0; j < (int)listaTMP[i].size(); j++)
+			if(listaTMP[i][j].waga != -1)
+				kolejka.push(listaTMP[i][j]);
+	}
+
+	vector <Krawedz> wynik;
+	int pIn, pOut, suma = 0;
+	int *parent = new int[liczbaWierzcholkow];
+	for(int i = 0; i < liczbaWierzcholkow; i++) parent[i] = i;
+	while(!kolejka.empty()) {
+		Krawedz k = kolejka.top();
+		pIn = znajdz(k.win, parent); 
+		pOut = znajdz(k.wout, parent);
+		if(pIn != pOut) {
+			wynik.push_back(k);
+			suma += k.waga;
+			parent[pIn] = parent[pOut];
+		}
+		kolejka.pop();
+	}
+
+	if(czyWyswietlacWyniki == true) {
+		wyswietlaniePolaczen(wynik);
+		cout << "SUMA: " << suma << endl;
+	}
+
+	return suma;
+}
+
+int znajdz(int x, int *parent) {
+	if(x != parent[x])
+		parent[x] = znajdz(parent[x], parent);
+	return parent[x];
+}
+
+int kruskalMacierz() {
+	Krawedz **macierzTMP = macierz;
+	kolejkaKP kolejka;
+	for(int  i = 0; i < liczbaWierzcholkow; i++)
+		for(int j = i; j < liczbaWierzcholkow; j++) 
+			if(macierzTMP[i][j].waga != -1)
+				kolejka.push(macierzTMP[i][j]);
+
+	vector <Krawedz> wynik;
+	int pIn, pOut, suma = 0;
+	int *parent = new int[liczbaWierzcholkow];
+	for(int i = 0; i < liczbaWierzcholkow; i++) parent[i] = i;
+	while(!kolejka.empty()) {
+		Krawedz k = kolejka.top();
+		pIn = znajdz(k.win, parent);
+		pOut = znajdz(k.wout, parent);
+		if(pIn != pOut) {
+			wynik.push_back(k);
+			suma += k.waga;
+			parent[pIn] = parent[pOut];
+		}
+		kolejka.pop();
+	}
+
+	if(czyWyswietlacWyniki == true) {
+		wyswietlaniePolaczen(wynik);
+		cout << "SUMA: " << suma << endl;
+	}
+
+	return suma;
+}
+
+void losowanieGrafow(double p) {
+	macierz = new Krawedz*[liczbaWierzcholkow];
+	for(int i = 0; i < liczbaWierzcholkow; i++)
+		macierz[i] = new Krawedz[liczbaWierzcholkow];
+	lista = vector< vector< Krawedz>>(liczbaWierzcholkow, vector<Krawedz> (0));
+
+	default_random_engine generator;
+	mt19937 gen;
+	int* wagi = new int[liczbaWierzcholkow * liczbaWierzcholkow];
+	for(int i = 0; i < liczbaWierzcholkow * liczbaWierzcholkow; i++) {
+		poisson_distribution <> pois_dist(10.0);
+		variate_generator<mt19937&, poisson_distribution<>> poisson_sampler(gen, pois_dist);
+		wagi[i] = poisson_sampler();
+	}
+	for(int i = 0; i <liczbaWierzcholkow; i++)
+		for(int j = i+1; j < liczbaWierzcholkow; j++) {
+			if(rand() % 100000 < p * 100000) {
+				Krawedz k(i,j,wagi[i*j]);
+				Krawedz k1(j,i,wagi[i*j]);
+				macierz[i][j] = k;
+				macierz[j][i] = k1;
+				lista[i].push_back(k);
+				lista[j].push_back(k1);
+				liczbaKrawedzi++;
+			}
+		}
+}
+
+void eksperyment() {
+	vector <long double> czasPl, czasPm, czasKl, czasKm;
+	std::fstream plik("wyniki.txt", std::fstream::out | std::fstream::app);
+	int wierzcholki[] = {500,1000,2000,4000};
+	__int64 czestotliosc;
+	QueryPerformanceFrequency((LARGE_INTEGER*) &czestotliosc);
+	for(int i = 0; i < 4; i++) {
+		liczbaWierzcholkow = wierzcholki[i];
+		double p[] = {3 * log((double)liczbaWierzcholkow)/liczbaWierzcholkow, pow((double)(liczbaWierzcholkow),(-1/3.0)), pow((double)(liczbaWierzcholkow),(-1/2.0)), 1};
+		for(int j = 0; j < 4; j++) {
+			if(plik.good()) 
+				plik << "\n liczbaWierzcholkow: " << liczbaWierzcholkow 
+				<< "  prawdopodobienstwo: " << p[j] << endl;
+			cout << "\n liczbaWierzcholkow: " << liczbaWierzcholkow 
+				<< "  prawdopodobienstwo: " << p[j] << endl;
+			for(int k = 0; k < 100; k++) {
+				if(k%10 == 9) {
+					cout << ".";
+				}
+				losowanieGrafow(p[j]);
+				__int64 poczatekPl = 0, koniecPl = 0, poczatekPm = 0, koniecPm = 0;
+				ustawianieFlagPrim();
+
+				QueryPerformanceCounter((LARGE_INTEGER*) &poczatekPl);
+				primLista();
+				QueryPerformanceCounter((LARGE_INTEGER*) &koniecPl);
+
+				ustawianieFlagPrim();
+
+				QueryPerformanceCounter((LARGE_INTEGER*) &poczatekPm);
+				primMacierz();
+				QueryPerformanceCounter((LARGE_INTEGER*) &koniecPm);
+
+				ustawianieFlagPrim();
+
+				czasPl.push_back((long double)((koniecPl - poczatekPl)*1000/czestotliosc));
+				czasPm.push_back((long double)((koniecPm - poczatekPm)*1000/czestotliosc));
+
+				__int64 poczatekKl = 0, koniecKl = 0, poczatekKm = 0, koniecKm = 0;
+
+				QueryPerformanceCounter((LARGE_INTEGER*) &poczatekKl);
+				kruskalLista();
+				QueryPerformanceCounter((LARGE_INTEGER*) &koniecKl);
+
+				QueryPerformanceCounter((LARGE_INTEGER*) &poczatekKm);
+				kruskalMacierz();
+				QueryPerformanceCounter((LARGE_INTEGER*) &koniecKm);
+
+				czasKl.push_back((long double)((koniecKl - poczatekKl)*1000/czestotliosc));
+				czasKm.push_back((long double)((koniecKm - poczatekKm)*1000/czestotliosc));
+			}
+			cout << endl;
+			plik << "Kruskal lista" << endl;
+			cout << "Kruskal lista" << endl;
+			usrednianieCzasu(czasKl);
+			plik << "Kruskal macierz" << endl;
+			cout << "Kruskal macierz" << endl;
+			usrednianieCzasu(czasKm);
+			plik << "Prim lista" << endl;
+			cout << "Prim lista" << endl;
+			usrednianieCzasu(czasPl);
+			plik << "Prim macierz" << endl;
+			cout << "Prim macierz" << endl;
+			usrednianieCzasu(czasPm);
+			czasKl.clear();
+			czasKm.clear();
+			czasPl.clear();
+			czasPm.clear();
+		}
+	}
+	plik.close();
+}
+
+void usrednianieCzasu(vector <long double> wektor) {
+	long double temp = 0;
+	for(int i = 0; i < (int)wektor.size(); i++) {
+		temp += wektor[i];
+	}
+	long double wynik = temp/wektor.size();
+	std::cout.setf(std::ios::fixed, std::ios::floatfield);
+	std::fstream plik("wyniki.txt", std::fstream::out | std::fstream::app);
+	if(plik.good()) plik << "\nSredni_czas(ms): " << wynik << "\n\n";
+	cout << "\nSredni_czas(ms): " << wynik << "\n\n";
+	plik.close();
+}
+
+void zapisanieLosowychGrafow() {
+	string nazwa;
+	float prawdopodobienstwo;
+	cout << "podaj liczbe wierzcholkow i prawdopodobienstwo (0. - 1)\n";
+	cin >> liczbaWierzcholkow >> prawdopodobienstwo;
+	nazwa = "graf" + to_string((long double)liczbaWierzcholkow) + "-" + to_string((long double)prawdopodobienstwo) + ".txt";
+	losowanieGrafow(prawdopodobienstwo);
+	fstream plik(nazwa, ios::out);
+	if(plik.good()) {
+		for(int i = 0; i < liczbaWierzcholkow; i++) {
+			for(int j = i; j < liczbaWierzcholkow; j++)
+				if(macierz[i][j].waga != -1)
+					plik << macierz[i][j].win << "\t" << macierz[i][j].wout << "\t" << macierz[i][j].waga << endl;
+		}
+	}
 }
